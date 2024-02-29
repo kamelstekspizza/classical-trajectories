976f3c1d9f66b207aaa64c9e199f51330789ce47
edvino <edvin.olofsson@matfys.lth.se>
2024-02-29 16:44:41 +0100

    Simulation and trajectory classes

---
+++ src/simulation.py
@@ -0,0 +1,114 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors
+from trajectory import Trajectory,Laser
+import concurrent.futures #Used for multiprocessing
+import os
+
+
+class Simulation:
+    def __init__(self,N_traj,E_0,omega,u,tau,pulse_type,t_0_limits,p_t_limits,**kwargs):
+        self.Laser = Laser(E_0,omega,u,tau,pulse_type)
+        self.t_0 = np.linspace(t_0_limits[0],t_0_limits[1],N_traj)
+        self.p_t = np.linspace(p_t_limits[0],p_t_limits[1],N_traj)
+        
+        #Keyword arguments
+        self.coulomb = kwargs.get('coulomb',True)
+        self.N_proc = kwargs.get('N_proc',1)
+
+    def energy_map(self):
+        fig,ax = plt.subplots()
+        colormap = plt.cm.turbo #or any other colormap
+        normalize = matplotlib.colors.Normalize(vmin=-0.5, vmax=0)
+
+        T_0,P_t = np.meshgrid(self.t_0,self.p_t)
+
+        self.t_0_list = []
+        self.p_t_list = []
+        self.energy_list = []
+
+        print()
+        print('Simulating trajectories... ')
+
+        
+        with concurrent.futures.ProcessPoolExecutor(max_workers = self.N_proc) as executor:
+            #for t_0 in self.t_0:
+            parameters = [[t_0,p_t] for t_0,p_t in np.nditer((T_0,P_t))]
+            #for p in parameters:
+            #    print(p)
+            results = [executor.submit(self.simulate,*parameter) for parameter in parameters]
+            for f in concurrent.futures.as_completed(results):
+                print('A calculation has finished')
+
+                if f.result()[2] < 0:
+                    self.t_0_list.append(f.result()[0])
+                    self.p_t_list.append(f.result()[1])
+                    self.energy_list.append(f.result()[2])
+                
+        """             for p_t in self.p_t:
+                trajectory = Trajectory(t_0,self.Laser.tau/2,p_t,self.Laser,coulomb = self.coulomb)
+                trajectory.solve()
+                if trajectory.final_energy < 0 and trajectory.final_energy > -0.5:
+                    self.t_0_list.append(t_0)
+                    self.p_t_list.append(p_t)
+                    self.energy_list.append(trajectory.final_energy)
+                    print(f'(t_0,p_t,E) = ({t_0},{p_t},{trajectory.final_energy})')
+                    #ax.scatter(t_0_list,p_t_list,c = energy_list,cmap = colormap,norm= normalize) """
+
+        sc = ax.scatter(self.t_0_list,self.p_t_list,c = self.energy_list,cmap = colormap,norm= normalize) #Make marker color depend on energy!
+        cb = fig.colorbar(sc)
+
+        self.save_output()
+
+        print('Done!')
+        print('')
+
+        #trajectory = Trajectory(t_0_list[0],self.Laser.tau/2,p_t_list[0],self.Laser,coulomb = self.coulomb,save_traj = True)
+        #trajectory.solve()
+        #trajectory.plot()
+
+        #trajectory = Trajectory(t_0_list[-1],self.Laser.tau/2,p_t_list[-1],self.Laser,coulomb = self.coulomb,save_traj = True)
+        #trajectory.solve()
+        #trajectory.plot()
+
+        return
+
+    def simulate(self,t_0,p_t):
+        trajectory = Trajectory(t_0,self.Laser.tau/2,p_t,self.Laser,coulomb = self.coulomb)
+        trajectory.solve()
+        if trajectory.final_energy < 0 and trajectory.final_energy > -0.5:
+            print(f'(t_0,p_t,E) = ({t_0},{p_t},{trajectory.final_energy})')
+            return (t_0,p_t,trajectory.final_energy)
+            
+        else:
+            return (t_0,p_t,10)
+        
+
+    def plot_E(self,time):
+        fig_E,ax_E = plt.subplots()
+        ax_E.plot(time,self.Laser.E(time))
+
+        return
+    
+    def make_outputfolder(self):
+        #Make a numbered directory XXXX in output directory to store calculation results
+        for i in range(1,10000):
+            this_run = '{:d}'.format(i).zfill(4)
+            output_folder = f'./OUTPUT/{this_run}'
+            if not(os.path.isdir((output_folder))):
+                os.makedirs(output_folder)
+                self.output_folder = output_folder
+                break
+        return
+
+    def save_output(self):
+        #Save the output of calculations, and copy input file to directory where output is stored
+        self.make_outputfolder()
+        print('')
+        print(f'Saving output in {self.output_folder}')
+        print('')
+
+        self.DATA = np.transpose(np.array([self.t_0_list,self.p_t_list,self.energy_list]))
+        np.savetxt(f'{self.output_folder}/output.dat',self.DATA,header = 't_0 [a.u.], p_t [a.u.], Energy [a.u.]')
+
+        return
---
+++ src/trajectory.py
@@ -0,0 +1,121 @@
+import numpy as np
+import scipy.integrate as si
+import matplotlib.pyplot as plt
+
+
+class Trajectory:
+    def __init__(self,t_0,t_end,p_t,Laser_in,**kwargs):
+        #Required arguments  
+        self.t_0 = t_0
+        self.t_end = t_end
+        self.p_t = p_t
+        self.Laser = Laser_in
+
+        #keyword arguments
+        self.coulomb = kwargs.get('coulomb',True)
+        self.save_traj = kwargs.get('save_traj',False)
+
+
+        if self.coulomb:
+            self.RHS = self.RHS_coulomb
+        else:
+            self.RHS = self.RHS_no_coulomb
+
+        return
+        
+
+    def RHS_coulomb(self,t,y):
+        r_3 = np.sqrt(y[0]**2+y[1]**2)**3
+        RHS = np.zeros(4)
+        RHS[0] = y[2]
+        RHS[1] = y[3]
+        RHS[2] = -y[0]/r_3 -self.Laser.E(t)
+        RHS[3] = -y[1]/r_3
+
+        return RHS
+    
+    def RHS_no_coulomb(self,t,y):
+        RHS = np.zeros(4)
+        RHS[0] = y[2]
+        RHS[1] = y[3]
+        RHS[2] = -self.Laser.E(t)
+        return RHS
+    
+    def energy(self,y):
+        r = np.sqrt(y[0]**2+y[1]**2)
+        potential = -1.0/r
+        kinetic = 0.5*(y[2]**2+y[3]**2)
+
+        return potential + kinetic
+
+    def solve(self):
+        if np.abs(self.Laser.E(self.t_0)) < 1e-9:
+            #Electric field is too small for tunneling!
+            self.final_energy  = 10
+            return
+        
+        limits = (self.t_0,self.t_end)
+        y_0 = np.zeros(4)
+        y_0[0] = (0.5 + 0.5*self.p_t**2)/self.Laser.E(self.t_0) #Effective tunneling barrier, 0.5 is the I_p of hydrogen
+        #y_0[0] = 0.5/self.Laser.E(self.t_0) #Effective tunneling barrier, 0.5 is the I_p of hydrogen
+        #y_0[2] = -self.Laser.A(self.t_0)
+        y_0[3] = self.p_t
+        solution = si.solve_ivp(self.RHS,limits,y_0,method = 'DOP853',max_step = 0.01*self.Laser.period)
+
+        
+
+        self.final_energy = self.energy(solution.y[:,-1])
+        
+        if self.save_traj:
+            self.time = solution.t
+            self.y = solution.y
+
+    def coulomb_orbit(self):
+        E = self.energy(self.y)
+        ang_mom = self.y[0]*self.y[3]
+        e = np.sqrt(1+ 2*E*ang_mom**2)
+
+    def plot(self):
+        fig_pos,ax_pos = plt.subplots()
+        fig_mom,ax_mom = plt.subplots()
+        fig_energy,ax_energy = plt.subplots()
+
+        ax_pos.plot(self.y[0],self.y[1])
+        ax_mom.plot(self.time,self.y[2])
+        ax_energy.plot(self.time,self.energy(self.y))
+
+        return
+    
+    
+    
+    
+
+
+class Laser:
+    def __init__(self,E_0,omega,u,tau,pulse_type):
+        self.E_0 = E_0
+        self.omega = omega
+        self.u = u
+        self.tau = tau
+        self.pulse_type = pulse_type
+
+        self.period = 2*np.pi/self.omega
+
+        if self.pulse_type == 'mono':
+            self.A = self.A_mono
+            self.E = self.E_mono
+        elif self.pulse_type == 'cos_2':
+            self.A = self.A_cos_2
+            self.E = self.E_cos_2
+
+    def A_mono(self,t):
+       return -self.E_0/self.omega*np.sin(self.omega*t)
+         
+    def A_cos_2(self,t):
+        return self.E_0/(self.omega)*np.sin(self.omega*t+self.u)*(np.cos(np.pi*t/self.tau))**2
+
+    def E_mono(self,t):
+        return self.E_0*np.cos(self.omega*t)
+    
+    def E_cos_2(self,t):
+        return self.E_0/(self.omega)*(-self.omega*np.cos(self.omega*t+self.u)*(np.cos(np.pi*t/self.tau))**2+2*np.pi/self.tau*np.sin(self.omega*t +self.u)*np.sin(np.pi*t/self.tau)*np.cos(np.pi*t/self.tau))
