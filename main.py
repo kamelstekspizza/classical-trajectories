Changed from concurrent.futures to multiprocessing for the parallel ODE integration. Fixed sign error in tunneling location. Implemented Gaussian and cos^6 envelope. Now also saves input to output folder. Added small script to plot the initial condition maps.

main.py
@@ -6,19 +6,20 @@ sys.path.append('./src/')
 from simulation import Simulation
 from trajectory import Trajectory
 
-N_traj = 10
-N_proc = 7
-E_0 = 6.5372045046E-02 #1.5e14 W/cm^2
-#E_0 = 0.076
+N_traj = 2000
+N_proc = 28
+#E_0 = 6.5372045046E-02 #1.5e14 W/cm^2
+#E_0 = 1.1003754863E-01 #4.25e14 W/cm^2
+E_0 = 0.076
 #E_0 = 0.01
 omega = 0.057
 period = 2*np.pi/omega
 u = 0
 tau = 4*period
 pulse_type = 'cos_2'
-t_0_limits = [-0.25*period,0.25*period]
-t_0_limits = [-30,30]
-p_t_limits = [0,0.3]
+#t_0_limits = [-0.20*period,0.20*period]
+t_0_limits = [-8,8]
+p_t_limits = [0,0.4]
 
 
 
@@ -28,31 +29,31 @@ sim.energy_map()
 sim = Simulation(N_traj,E_0,omega,u,tau,pulse_type,t_0_limits,p_t_limits,coulomb = False,N_proc = N_proc)
 sim.energy_map()
 
-time = np.linspace(-tau/2,tau/2,200)
-sim.plot_E(time)
+#time = np.linspace(-tau*6,tau*6,2000)
+#sim.plot_E(time)
 
-traj = Trajectory(-2.3,tau/2,0.05,sim.Laser,save_traj = True)
-traj.solve()
-traj.plot()
-print(traj.energy(traj.y[:,0]))
-print(traj.final_energy)
+#traj = Trajectory(-2.3,6*tau,0.27,sim.Laser,save_traj = True)
+#traj.solve()
+#traj.plot()
+#print(traj.energy(traj.y[:,0]))
+#print(traj.final_energy)
 
-traj = Trajectory(-2.3,tau/2,0.05,sim.Laser,save_traj = True, coulomb = False)
-traj.solve()
-traj.plot()
-print(traj.energy(traj.y[:,0]))
-print(traj.final_energy)
+#traj = Trajectory(-2.3,tau/2,0.05,sim.Laser,save_traj = True, coulomb = False)
+#traj.solve()
+#traj.plot()
+#print(traj.energy(traj.y[:,0]))
+#print(traj.final_energy)
 
-traj = Trajectory(2.3,tau/2,0.05,sim.Laser,save_traj = True)
-traj.solve()
-traj.plot()
-print(traj.energy(traj.y[:,0]))
-print(traj.final_energy)
+#traj = Trajectory(2.3,6*tau,0.27,sim.Laser,save_traj = True)
+#traj.solve()
+#traj.plot()
+#print(traj.energy(traj.y[:,0]))
+#print(traj.final_energy)
 
-traj = Trajectory(2.3,tau/2,0.05,sim.Laser,save_traj = True,coulomb = False)
-traj.solve()
-traj.plot()
-print(traj.energy(traj.y[:,0]))
-print(traj.final_energy)
+#traj = Trajectory(2.3,6*tau,0.27,sim.Laser,save_traj = True,coulomb = False)
+#traj.solve()
+#traj.plot()
+#print(traj.energy(traj.y[:,0]))
+#print(traj.final_energy)
 
 plt.show()

src/simulation.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 import matplotlib.colors
 from trajectory import Trajectory,Laser
 import concurrent.futures #Used for multiprocessing
+from multiprocessing import Pool
 import os
-
+import time
 
 class Simulation:
     def __init__(self,N_traj,E_0,omega,u,tau,pulse_type,t_0_limits,p_t_limits,**kwargs):
@@ -16,13 +17,15 @@ class Simulation:
         self.coulomb = kwargs.get('coulomb',True)
         self.N_proc = kwargs.get('N_proc',1)
 
+        self.input = [N_traj,E_0,omega,u,tau,pulse_type,t_0_limits,p_t_limits,self.coulomb,self.N_proc]
+
     def energy_map(self):
-        fig,ax = plt.subplots()
-        colormap = plt.cm.turbo #or any other colormap
-        normalize = matplotlib.colors.Normalize(vmin=-0.5, vmax=0)
+        #fig,ax = plt.subplots()
+        #colormap = plt.cm.jet #or any other colormap
+        #normalize = matplotlib.colors.Normalize(vmin=-0.5, vmax=0)
 
         T_0,P_t = np.meshgrid(self.t_0,self.p_t)
-
+        parameters = [(t_0,p_t) for t_0,p_t in np.nditer((T_0,P_t))]
         self.t_0_list = []
         self.p_t_list = []
         self.energy_list = []
@@ -30,20 +33,34 @@ class Simulation:
         print()
         print('Simulating trajectories... ')
 
+        time_start = time.time()
+        with Pool(processes = self.N_proc) as pool:
+            results = pool.starmap(self.simulate,parameters)
         
-        with concurrent.futures.ProcessPoolExecutor(max_workers = self.N_proc) as executor:
+        for result in results:
+            if result[2] < 0:
+                self.t_0_list.append(result[0])
+                self.p_t_list.append(result[1])
+                self.energy_list.append(result[2])
+
+        time_end = time.time()
+        print()
+        print(f'Wall time: {time_end-time_start}')
+
+        #time_start = time.time()
+        #with concurrent.futures.ProcessPoolExecutor(max_workers = self.N_proc) as executor:
             #for t_0 in self.t_0:
-            parameters = [[t_0,p_t] for t_0,p_t in np.nditer((T_0,P_t))]
+            #parameters = [[t_0,p_t] for t_0,p_t in np.nditer((T_0,P_t))]
             #for p in parameters:
             #    print(p)
-            results = [executor.submit(self.simulate,*parameter) for parameter in parameters]
-            for f in concurrent.futures.as_completed(results):
-                print('A calculation has finished')
+            #results = [executor.submit(self.simulate,*parameter) for parameter in parameters]
+            #for f in concurrent.futures.as_completed(results):
+                #print('A calculation has finished')
 
-                if f.result()[2] < 0:
-                    self.t_0_list.append(f.result()[0])
-                    self.p_t_list.append(f.result()[1])
-                    self.energy_list.append(f.result()[2])
+             #   if f.result()[2] < 0:
+             #       self.t_0_list.append(f.result()[0]*self.Laser.omega/np.pi*180)
+             #       self.p_t_list.append(f.result()[1])
+             #       self.energy_list.append(f.result()[2])
                 
         """             for p_t in self.p_t:
                 trajectory = Trajectory(t_0,self.Laser.tau/2,p_t,self.Laser,coulomb = self.coulomb)
@@ -55,8 +72,11 @@ class Simulation:
                     print(f'(t_0,p_t,E) = ({t_0},{p_t},{trajectory.final_energy})')
                     #ax.scatter(t_0_list,p_t_list,c = energy_list,cmap = colormap,norm= normalize) """
 
-        sc = ax.scatter(self.t_0_list,self.p_t_list,c = self.energy_list,cmap = colormap,norm= normalize) #Make marker color depend on energy!
-        cb = fig.colorbar(sc)
+        #time_end = time.time()
+        #print(f'Wall time: {time_end-time_start}')
+
+        #sc = ax.scatter(self.t_0_list,self.p_t_list,c = self.energy_list,cmap = colormap,norm= normalize,s = 1) #Make marker color depend on energy!
+        #cb = fig.colorbar(sc)
 
         self.save_output()
 
@@ -74,10 +94,13 @@ class Simulation:
         return
 
     def simulate(self,t_0,p_t):
-        trajectory = Trajectory(t_0,self.Laser.tau/2,p_t,self.Laser,coulomb = self.coulomb)
+        if self.Laser.pulse_type == 'gaus':
+            trajectory = Trajectory(t_0,self.Laser.tau*6,p_t,self.Laser,coulomb = self.coulomb)
+        else:
+            trajectory = Trajectory(t_0,self.Laser.tau/2,p_t,self.Laser,coulomb = self.coulomb)
         trajectory.solve()
-        if trajectory.final_energy < 0 and trajectory.final_energy > -0.5:
-            print(f'(t_0,p_t,E) = ({t_0},{p_t},{trajectory.final_energy})')
+        if trajectory.final_energy < 0:# and trajectory.final_energy > -0.5:
+            #print(f'(t_0,p_t,E) = ({t_0},{p_t},{trajectory.final_energy})')
             return (t_0,p_t,trajectory.final_energy)
             
         else:
@@ -110,5 +133,9 @@ class Simulation:
 
         self.DATA = np.transpose(np.array([self.t_0_list,self.p_t_list,self.energy_list]))
         np.savetxt(f'{self.output_folder}/output.dat',self.DATA,header = 't_0 [a.u.], p_t [a.u.], Energy [a.u.]')
+
+        with open(f'{self.output_folder}/input.dat','w') as file:
+            for line in self.input:
+                file.write(str(line)+'\n')
 
         return

src/trajectory.py
@@ -56,11 +56,12 @@ class Trajectory:
         
         limits = (self.t_0,self.t_end)
         y_0 = np.zeros(4)
-        y_0[0] = (0.5 + 0.5*self.p_t**2)/self.Laser.E(self.t_0) #Effective tunneling barrier, 0.5 is the I_p of hydrogen
+        y_0[0] = -(0.5 + 0.5*self.p_t**2)/self.Laser.E(self.t_0) #Effective tunneling barrier, 0.5 is the I_p of hydrogen
+        #y_0[0] = -(0.5 + np.sqrt(0.5**2-4*self.Laser.E(self.t_0)))/(2*self.Laser.E(self.t_0)) #Effective tunneling barrier, 0.5 is the I_p of hydrogen, Eichmann papers
         #y_0[0] = 0.5/self.Laser.E(self.t_0) #Effective tunneling barrier, 0.5 is the I_p of hydrogen
         #y_0[2] = -self.Laser.A(self.t_0)
         y_0[3] = self.p_t
-        solution = si.solve_ivp(self.RHS,limits,y_0,method = 'DOP853',max_step = 0.01*self.Laser.period)
+        solution = si.solve_ivp(self.RHS,limits,y_0,method = 'RK45',max_step = 0.25*self.Laser.period)
 
         
 
@@ -99,6 +100,7 @@ class Laser:
         self.tau = tau
         self.pulse_type = pulse_type
 
+        self.A_0 = self.E_0/self.omega
         self.period = 2*np.pi/self.omega
 
         if self.pulse_type == 'mono':
@@ -106,7 +108,14 @@ class Laser:
             self.E = self.E_mono
         elif self.pulse_type == 'cos_2':
             self.A = self.A_cos_2
-            self.E = self.E_cos_2
+            self.E = self.E_cos_2        
+        elif self.pulse_type == 'cos_6':
+            self.A = self.A_cos_6
+            self.E = self.E_cos_6
+
+        elif self.pulse_type == 'gaus':
+            self.A = self.A_gaus
+            self.E = self.E_gaus
 
     def A_mono(self,t):
        return -self.E_0/self.omega*np.sin(self.omega*t)
@@ -114,8 +123,21 @@ class Laser:
     def A_cos_2(self,t):
         return self.E_0/(self.omega)*np.sin(self.omega*t+self.u)*(np.cos(np.pi*t/self.tau))**2
 
+    def A_cos_6(self,t):
+        return self.E_0/(self.omega)*np.sin(self.omega*t+self.u)*(np.cos(np.pi*t/self.tau))**6
+
+    def A_gaus(self,t):
+        return self.A_0*np.sin(self.omega*t+self.u)*np.exp(-2.0*np.log(2)*(t/self.tau)**2)
+
     def E_mono(self,t):
         return self.E_0*np.cos(self.omega*t)
     
     def E_cos_2(self,t):
         return self.E_0/(self.omega)*(-self.omega*np.cos(self.omega*t+self.u)*(np.cos(np.pi*t/self.tau))**2+2*np.pi/self.tau*np.sin(self.omega*t +self.u)*np.sin(np.pi*t/self.tau)*np.cos(np.pi*t/self.tau))
+
+    def E_cos_6(self,t):
+        return self.E_0/(self.omega)*(-self.omega*np.cos(self.omega*t+self.u)*(np.cos(np.pi*t/self.tau))**2+6*np.pi/self.tau*np.sin(self.omega*t +self.u)*np.sin(np.pi*t/self.tau)*np.cos(np.pi*t/self.tau)**5)
+
+    def E_gaus(self,t):
+        return self.A_0*np.exp(-2.0*np.log(2)*(t/self.tau)**2)*(-self.omega*np.cos(self.omega*t+self.u) + 4.0*np.log(2)*t/self.tau**2*np.sin(self.omega*t+self.u))
+